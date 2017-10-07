f4f6e623d1a2c53a7f8271366b0739a8d1866e11
Nagumkc <nandigam.nag@gmail.com>
2017-10-07 13:24:19 -0500

    commit

---
+++ Heron/HeronTridentWordCount/src/main/java/StormMain.java
@@ -0,0 +1,79 @@
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.log4j.BasicConfigurator;
+/**
+ * Created by Mayanka on 16-Sep-15.
+ */
+public class StormMain {
+    public static void main(String args[]) {
+
+
+        BasicConfigurator.configure();
+
+        if (args != null && args.length > 0) {
+            try {
+                StormSubmitter.submitTopology(args[0],createConfig(false),createTopology());
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {
+            try{
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("word-count",createConfig(true),createTopology());
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            cluster.shutdown();
+        }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+
+
+    private static StormTopology createTopology()
+    {
+
+        TopologyBuilder topology = new TopologyBuilder();
+
+/**Sample Text Spout**/
+
+        //  topology.setSpout("SampleSpout", new StormSpout(), 4);
+
+        // topology.setBolt("SplitSentence", new SplitSentenceBolt(), 4).shuffleGrouping("SampleSpout");
+
+/**Twitter Spout**/
+        topology.setSpout("TwitterSpout",new TwitterSpout(),4);
+
+        topology.setBolt("SplitSentence", new SplitSentenceBolt(), 4).shuffleGrouping("TwitterSpout");
+
+
+        topology.setBolt("WordCount",new WordCountBolt(),4).shuffleGrouping("SplitSentence");
+
+        return topology.createTopology();
+    }
+
+    private static Config createConfig(boolean local)
+    {
+        int workers = 1;
+        Config conf = new Config();
+        conf.setDebug(true);
+        if (local)
+            conf.setMaxTaskParallelism(workers);
+        else
+            conf.setNumWorkers(workers);
+        return conf;
+    }
+
+}
---
+++ Heron/HeronTridentWordCount/src/main/java/TwitterSpout.java
@@ -0,0 +1,94 @@
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by Mayanka on 16-Sep-15.
+ */
+public class TwitterSpout extends BaseRichSpout {
+    SpoutOutputCollector _collector;
+    LinkedBlockingQueue<Status> queue = null;
+    TwitterStream _twitterStream;
+    String _username;
+    String _pwd;
+    String consumer_key="bjnYTbDvOd61oShbWWyaD2leP";
+    String consumer_secret="mCy4iONQfevhJKmfqK1Kt0GjHb1uG0z2f3VUrhN3upmPZMXgsp";
+    String access_token="434908233-C896jBM6t1Iq7ta0FJ93YQeD3GP2439T1lidFXyR";
+    String token_secret="JTPffFFLE6Qk1j4c2YTlDeeF8kkjop1Vtw4ANanVS1ivx";
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+            outputFieldsDeclarer.declare(new Fields("Status"));
+    }
+
+    public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+        queue = new LinkedBlockingQueue<Status>(1000);
+        _collector = spoutOutputCollector;
+        StatusListener listener = new StatusListener() {
+
+            public void onException(Exception e) {
+
+            }
+
+            public void onStatus(Status status) {
+                queue.offer(status);
+            }
+
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+
+            }
+
+            public void onTrackLimitationNotice(int i) {
+
+            }
+
+            public void onScrubGeo(long l, long l1) {
+
+            }
+
+            public void onStallWarning(StallWarning stallWarning) {
+
+            }
+        };
+
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        TwitterStreamFactory fact = new TwitterStreamFactory();
+        _twitterStream = fact.getInstance();
+        _twitterStream.setOAuthConsumer(consumer_key, consumer_secret);
+        _twitterStream.setOAuthAccessToken(new AccessToken(access_token, token_secret));
+        _twitterStream.addListener(listener);
+        _twitterStream.sample();
+    }
+
+    public void nextTuple() {
+        Status ret = queue.poll();
+        if(ret==null) {
+            Utils.sleep(50);
+        } else {
+           String s=ret.getText();
+
+           //collecting tweets in file
+            try {
+                BufferedWriter br = new BufferedWriter(new FileWriter(new File("input"), true));
+                br.append(s+ "\n");
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // emit tuple to next bolt
+            _collector.emit(new Values(s));
+        }
+    }
+}
